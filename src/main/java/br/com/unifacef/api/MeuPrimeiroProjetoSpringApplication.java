eeee4cc3378ad0b17c381e3c9dd14071008c1452
Tamara Carreiras <tamara.carreiras@hotmail.com>
2019-08-13 22:18:15 -0300

    Primeira versão

--- src/main/java/br/com/unifacef/api/MeuPrimeiroProjetoSpringApplication.java
+++ src/main/java/br/com/unifacef/api/MeuPrimeiroProjetoSpringApplication.java
@@ -8,6 +8,7 @@ public class MeuPrimeiroProjetoSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeuPrimeiroProjetoSpringApplication.class, args);
+		System.out.println('Esta é uma alteração no projeto');
 	}
 
 }
