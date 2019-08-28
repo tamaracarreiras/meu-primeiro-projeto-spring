7d602256c2e78059746d715ecb3b54060e20b8ad
Tamara Carreiras <tamara.carreiras@hotmail.com>
2019-08-27 22:26:22 -0300

    Atualizado

--- src/main/java/br/com/unifacef/api/MeuPrimeiroProjetoSpringApplication.java
+++ src/main/java/br/com/unifacef/api/MeuPrimeiroProjetoSpringApplication.java
@@ -1,14 +1,27 @@
 package br.com.unifacef.api;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MeuPrimeiroProjetoSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeuPrimeiroProjetoSpringApplication.class, args);
-		System.out.println("Esta é uma alteração no projeto");
+		System.out.println("Alteramos o arquivo application.properties");
+		System.out.println("de modo que não precisemos configurar agora a fonte de dados");
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			String senhaEncoded = SenhaUtils.gerarBCrypt("123456");
+			System.out.println("Senha encoded: " + senhaEncoded);
+			senhaEncoded = SenhaUtils.gerarBCrypt("123456");
+			System.out.println("Senha encoded novamente:" + senhaEncoded);
+			System.out.println("Senha válida: " + SenhaUtils.senhaValida("123456", senhaEncoded));
+		};
+	}
 }
